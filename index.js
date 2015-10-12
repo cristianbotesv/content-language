9cceabf5008716f0a8a5ea0da5d08a4dd9a98549
Cristian Bote <cristian.bote@softvision.ro>
2015-10-12 09:48:27 +0300

    Added CORS support

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var http = require('http'),
 server = http.createServer(function(req, res) {
     var contentLanguage = req.headers['accept-language'];
     res.setHeader('Content-type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.end(JSON.stringify({
         acceptLanguage: contentLanguage
     }));
